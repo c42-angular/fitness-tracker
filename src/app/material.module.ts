fe778d36730a60e5df6c81a81525d793f191c6df
Arunkumar Ramachandriya <arunkumar@hawesandcurtis.com>
2018-10-23 08:44:09 +0100

    Added Sidebar and a sample button to open/close it

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from "@angular/core";
-import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule } from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
-    exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
+    imports: [
+        MatButtonModule, 
+        MatIconModule, 
+        MatFormFieldModule, 
+        MatInputModule, 
+        MatDatepickerModule, 
+        MatNativeDateModule, 
+        MatCheckboxModule,
+        MatSidenavModule],
+    exports: [
+        MatButtonModule, 
+        MatIconModule, 
+        MatFormFieldModule, 
+        MatInputModule, 
+        MatDatepickerModule, 
+        MatNativeDateModule,
+        MatCheckboxModule,
+        MatSidenavModule]
 })
 export class MaterialModule {
 
