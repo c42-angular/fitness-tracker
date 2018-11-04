171ec9f53f86c8a3281c1297286f8a2416e5dcb9
Arunkumar Ramachandriya <cloud.fortytwo@gmail.com>
2018-11-04 11:01:18 +0000

    Listen to AngularFireStore's collection.snapshotChanges() - gives us ID

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,4 +1,5 @@
 import { Subject } from "rxjs";
+import { map } from 'rxjs/operators';
 import { AngularFirestore } from "@angular/fire/firestore";
 
 import { Training } from "./training.model";
@@ -7,19 +8,35 @@ import { Injectable } from "@angular/core";
 @Injectable()
 export class TrainingService {
     public trainingChanged = new Subject<Training>();
-    private availableTrainings: Training[] = [
-        { id: 1, name: 'Crunches', duration: 30, calories: 100 },
-        { id: 2, name: 'Squats', duration: 120, calories: 300 },
-        { id: 3, name: 'Sit-ups', duration: 300, calories: 500 },
-        { id: 4, name: 'Yoga', duration: 600, calories: 275 }
-    ];
+    private availableTrainings: Training[] = [];
+    //     { id: 1, name: 'Crunches', duration: 30, calories: 100 },
+    //     { id: 2, name: 'Squats', duration: 120, calories: 300 },
+    //     { id: 3, name: 'Sit-ups', duration: 300, calories: 500 },
+    //     { id: 4, name: 'Yoga', duration: 600, calories: 275 }
+    // ];
     private pastTrainings: Training[] = [];
     private currentTraining: Training;
 
     constructor(db: AngularFirestore){
-        db.collection('availableTrainings').valueChanges().subscribe(result => {
-            console.log(result);
-        });
+       db.collection('availableTrainings')
+            .snapshotChanges()
+            .pipe(
+                map(actions => {
+                    return actions.map(a => {
+                        const data = a.payload.doc.data();
+                        const id = a.payload.doc.id;
+                        return {
+                            id, 
+                            name: data['name'], 
+                            calories: data['calories'], 
+                            duration: data['duration']
+                        };
+                    }
+                )}
+            )
+            ).subscribe(trainings => {
+                console.log(trainings);
+            });        
     }
 
     getAvailableTrainings() {
