754b43b273d46279c09984bb5425425ccd3be62b
Arunkumar Ramachandriya <arunkumar@hawesandcurtis.com>
2018-11-12 14:42:07 +0000

    Store injected as type  <fromRoot.State> seems to work ({ui:  fromRoot.State}) earlier

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -21,7 +21,7 @@ export class LoginComponent implements OnInit {
   emailInput = new FormControl("", [Validators.required, Validators.email]);
   passwordInput = new FormControl("", Validators.required);
 
-  constructor(fb: FormBuilder, private authService: AuthService, private store: Store<{ui: fromRoot.State}>) {
+  constructor(fb: FormBuilder, private authService: AuthService, private store: Store<fromRoot.State>) {
     this.loginForm = fb.group({
       email: this.emailInput,
       password: this.passwordInput
