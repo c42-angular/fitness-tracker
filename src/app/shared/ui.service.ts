8f3f4e9f9dfec05a1b97972d9671f0f0480bd774
Arunkumar Ramachandriya <arunkumar@hawesandcurtis.com>
2018-11-12 12:03:54 +0000

    signUp component listens to UI Store instead of uiService subscriptions

--- src/app/shared/ui.service.ts
+++ src/app/shared/ui.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from "@angular/core";
 import { MatSnackBar } from "@angular/material";
-import { Subject } from "rxjs";
 
 @Injectable()
 export class UiService {
-    loadingStateChanged = new Subject<boolean>();
-    
     constructor(private snackBar: MatSnackBar){}
 
     showSnackBar(message: string, duration: number) {
